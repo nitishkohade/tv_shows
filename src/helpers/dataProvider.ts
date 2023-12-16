102af66d741f7495b7a439d5e3d1634536e62fae
nitishkohade <8028627+nitishkohade@users.noreply.github.com>
2023-12-17 00:26:06 +0100

    Adding data providers for calling backend api

---
+++ src/helpers/dataProvider.ts
@@ -0,0 +1,32 @@
+export const API_BASE_URL = "http://www.tvmaze.com/api";
+export const UNKNOWN_ERROR_MESSAGE = "Something went wrong";
+
+const handleApiError = async (response: Response) => {
+  if (!response.ok) {
+    let backendErrorMessage = UNKNOWN_ERROR_MESSAGE;
+
+    try {
+      // Should parse the backend error message from the response body
+      const errorData = await response.json();
+      if (errorData?.message) {
+        backendErrorMessage = errorData.message;
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        backendErrorMessage = error?.message;
+      }
+    }
+
+    throw new Error(backendErrorMessage);
+  }
+};
+
+export const fetchApiData = async <T>(url: string): Promise<T> => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/${url}`);
+    await handleApiError(response);
+    return await response.json();
+  } catch (err: unknown) {
+    throw new Error(err instanceof Error ? err.message : UNKNOWN_ERROR_MESSAGE);
+  }
+};
