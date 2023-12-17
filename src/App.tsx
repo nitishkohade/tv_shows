17dc98f30f67d633b86f92f9d7a9beb934362595
nitishkohade <8028627+nitishkohade@users.noreply.github.com>
2023-12-17 14:51:15 +0100

    Adding lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import {
-  ShowsPage,
-  ShowDetailPage,
-  EpisodeDetailPage,
-  NotFoundPage,
-} from "./pages";
+
+const ShowsPage = lazy(() =>
+  import("./pages").then((module) => ({ default: module.ShowsPage })),
+);
+const ShowDetailPage = lazy(() =>
+  import("./pages").then((module) => ({
+    default: module.ShowDetailPage,
+  })),
+);
+const EpisodeDetailPage = lazy(() =>
+  import("./pages").then((module) => ({
+    default: module.EpisodeDetailPage,
+  })),
+);
+const NotFoundPage = lazy(() =>
+  import("./pages").then((module) => ({
+    default: module.NotFoundPage,
+  })),
+);
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ShowsPage />} />
-      <Route path="/show/:showId">
-        <Route index element={<ShowDetailPage />} />
-        <Route path="episode/:episodeId" element={<EpisodeDetailPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading Content</div>}>
+      <Routes>
+        <Route path="/" element={<ShowsPage />} />
+        <Route path="/show/:showId">
+          <Route index element={<ShowDetailPage />} />
+          <Route path="episode/:episodeId" element={<EpisodeDetailPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
